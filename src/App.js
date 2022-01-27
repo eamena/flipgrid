9a0b6ff4f332f1504cbbb257d75fbfc4721c3e95
Esau <eamena@icloud.com>
2022-01-26 19:46:44 -0700

    Form UI completed

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { Field, Form, Formik } from "formik";
 function App() {
   const setValues = () => {};
   return (
-    <div className="d-flex stify-content-center align-items-center">
+    <div>
       <Card>
         <Card.Body>
-          <Card.Title>Let's</Card.Title>
-          <h1>Sign Up</h1>
+          <div className="card__title">Let's</div>
+          <h2>Sign Up</h2>
           <div>
             <p>
               Use the form below to sign up for this super awesome service.
@@ -25,13 +25,17 @@ function App() {
               onSubmit={setValues()}
             >
               <Form>
-                <label>First Name</label>
-                <Field id="firstName" name="firstName" />
-                <label>Email Address</label>
-                <Field id="email" name="email" type="email" />
-                <label>Password</label>
-                <Field id="Password" name="Password" />
-                <button type="submit">Sign Up</button>
+                <label>first name</label>
+                <Field id="firstName" name="firstName" required />
+                <label>email Address</label>
+                <Field id="email" name="email" type="email" required />
+                <label>password</label>
+                <Field id="Password" name="Password" required />
+                <div className="position-relative my-4">
+                  <button className="submit" type="submit">
+                    Sign Up
+                  </button>
+                </div>
               </Form>
             </Formik>
           </div>
