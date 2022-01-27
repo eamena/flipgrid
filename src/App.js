465a7ccd7d81b9a3368312a5ef3b589e5a4c1c43
Esau <eamena@icloud.com>
2022-01-26 18:51:03 -0700

    First part of signup form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Card } from "react-bootstrap";
+import { Field, Form, Formik } from "formik";
 
 function App() {
+  const setValues = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex stify-content-center align-items-center">
+      <Card>
+        <Card.Body>
+          <Card.Title>Let's</Card.Title>
+          <h1>Sign Up</h1>
+          <div>
+            <p>
+              Use the form below to sign up for this super awesome service.
+              You're only a few steps away!
+            </p>
+            <Formik
+              initialValues={{
+                firstName: "",
+                email: "",
+                password: "",
+              }}
+              onSubmit={setValues()}
+            >
+              <Form>
+                <label>First Name</label>
+                <Field id="firstName" name="firstName" />
+                <label>Email Address</label>
+                <Field id="email" name="email" type="email" />
+                <label>Password</label>
+                <Field id="Password" name="Password" />
+                <button type="submit">Sign Up</button>
+              </Form>
+            </Formik>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
